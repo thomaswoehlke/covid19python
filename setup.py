74266b0cd906ea0fc89c630667b90322e8c3ec58
thomaswoehlke <thomas.woehlke@gmail.com>
2021-01-22 16:24:57 +0100

    ### 0.0.11 Release * Issue #43 /europe/date_reported * Issue #44 /europe/continent * Issue #45 /europe/country * Issue #46 /europe/data * Issue #39 SQLalchemy instead of SQL: AllModelClasses.remove_all() * Issue #40 SQLalchemy instead of SQL: EuropeDataImportTable.get_date_rep() * Issue #41 SQLalchemy instead of SQL: EuropeDataImportTable.get_countries_of_continent() * Issue #42 SQLalchemy instead of SQL: WhoGlobalDataImportTable.get_new_dates_as_array() * Issue #47 refactor Routes from app.py to org...who,europe,... * Issue #48 refactor Tasks from server_mq.py to org...who,europe,...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='covid19python',
-    version='0.0.10',
+    version='0.0.11',
     packages=['org', 'org.woehlke', 'org.woehlke.covid19', 'org.woehlke.covid19.nrw', 'org.woehlke.covid19.rki',
               'org.woehlke.covid19.who', 'org.woehlke.covid19.admin', 'org.woehlke.covid19.europe'],
     url='https://github.com/thomaswoehlke/covid19python',
