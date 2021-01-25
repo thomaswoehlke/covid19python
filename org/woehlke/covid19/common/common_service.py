ee938f939a393653a108bb771a8d626e6fe4dcae
thomaswoehlke <thomas.woehlke@gmail.com>
2021-01-25 13:36:01 +0100

    work

--- org/woehlke/covid19/common/common_service.py
+++ org/woehlke/covid19/common/common_service.py
@@ -2,7 +2,6 @@ import os
 from database import app
 from org.woehlke.covid19.vaccination.vaccination_service_download import VaccinationServiceDownload
 from org.woehlke.covid19.vaccination.vaccination_service_import import VaccinationServiceImport
-from org.woehlke.covid19.vaccination.vaccination_service_update import VaccinationServiceUpdate
 
 common_service = None
 
@@ -16,7 +15,6 @@ class CommonService:
         self.limit_nr = 20
         self.vaccination_service_download = VaccinationServiceDownload(database)
         self.vaccination_service_import = VaccinationServiceImport(database)
-        self.vaccination_service_update = VaccinationServiceUpdate(database)
         app.logger.info("------------------------------------------------------------")
         app.logger.info(" Common Service [ready]")
 
