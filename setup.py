baaad929c9c85de973571c1a127208db8e6bd8a3
thomaswoehlke <thomas.woehlke@gmail.com>
2021-01-29 14:33:36 +0100

    work

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='app',
+    name='covid19',
     version='0.0.13',
     packages=find_packages(),
     url='https://github.com/thomaswoehlke/covid19python',
