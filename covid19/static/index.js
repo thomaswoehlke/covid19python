0cee7a3fe8d68d57d7fe068f39c4d93a047ec306
thomaswoehlke <thomas.woehlke@gmail.com>
2021-01-30 15:21:50 +0100

    Work

--- covid19/static/index.js
+++ covid19/static/index.js
@@ -1,4 +1,4 @@
-import 'node_modules/jquery/dist/jquery.min.js'
-import 'node_modules/popper.js/dist/popper.min.js'
-import 'node_modules/bootstrap/dist/js/bootstrap.min.js'
-import 'node_modules/@fortawesome/fontawesome-free/js/all.min.js'
+import '/node_modules/jquery/dist/jquery.min.js'
+import '/node_modules/popper.js/dist/popper.min.js'
+import '/node_modules/bootstrap/dist/js/bootstrap.min.js'
+import '/node_modules/@fortawesome/fontawesome-free/js/all.min.js'
