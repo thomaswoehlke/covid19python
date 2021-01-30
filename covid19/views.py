2457d7a112948f9cd80ceb516577385f792e148b
thomaswoehlke <thomas.woehlke@gmail.com>
2021-01-30 19:57:28 +0100

    ### 0.0.13 Release * Fixed #49 EuropeServiceUpdate.__update_data_short() (wontfix) * Fixed #52 download vaccination timeline data file * Fixed #53 import vaccination timeline data file into db * Fixed #54 /vaccination/imported * Fixed #57 frontend: use npm for handling 3rdParty css and javascript modules like jQuery, Bootstrap * Fixed #58 frontend: remove jumbotron from all pageheader, put jumbotron as main content on home page * Fixed #47 major refactoring: Routes from app.py to org...who,europe,... (Doublette von #65) * Fixed #48 major refactoring: Tasks from server_mq.py to org...who,europe,... (Doublette von #65) * Fixed #64 major refactoring: create two packages: for web app and for celery worker * Issue #65 major refactoring: add flask-blueprints for admin, common, europe, rki, vaccination, who
    ### 0.0.14 Release
    * Issue #66 frontend: migrate to Bootstrap Theme sb-admin-angular
    * Issue #67 implement Flask-Login
    
    ### 0.0.15 Release
    * Issue #5 Visual Graphs for Data per Countries order by Date
    * Issue #59 frontend: add correct breadcrumb to every page
    * Issue #60 frontend: better design for tables
    * Issue #61 frontend: better design for navtabs
    * Issue #62 frontend: better design for pages
    * Issue #63 frontend: add footer design
    
    ### 0.0.16 Release
    * Issue #28 /admin/database/import
    * Issue #39 SQLalchemy instead of SQL: AllModelClasses.remove_all()
    * Issue #40 SQLalchemy instead of SQL: EuropeDataImportTable.get_date_rep()
    * Issue #41 SQLalchemy instead of SQL: EuropeDataImportTable.get_countries_of_continent()
    * Issue #42 SQLalchemy instead of SQL: WhoGlobalDataImportTable.get_new_dates_as_array()

--- covid19/views.py
+++ covid19/views.py
@@ -2,11 +2,11 @@ from flask import render_template, redirect, url_for
 from database import app
 from covid19.oodm.common.common_model_transient import ApplicationPage
 
-import covid19.ui.who.views_who
-import covid19.ui.europe.views_europe
-import covid19.ui.vaccination.views_vaccination
-import covid19.ui.rki.views_rki
-import covid19.ui.admin.views_admin
+import covid19.oodm.who.views_who
+import covid19.oodm.europe.views_europe
+import covid19.oodm.vaccination.views_vaccination
+import covid19.oodm.rki.views_rki
+import covid19.oodm.admin.views_admin
 
 
 ############################################################################################
