f15717c592f4bf96f2e04781accffb45dea9cebc
thomaswoehlke <thomas.woehlke@gmail.com>
2021-02-05 14:18:55 +0100

    dev for Milestone 0.0.14

--- run_web.sh
+++ run_web.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 . venv/bin/activate
-#export FLASK_ENV=development
-#export FLASK_APP=covid19python
-#pip install -e .
-#flask run
+export FLASK_ENV=development
+export FLASK_APP=covid19
+pip install -e .
+flask run
 
 
-python covid19.__init__
+#python covid19.__init__
 
