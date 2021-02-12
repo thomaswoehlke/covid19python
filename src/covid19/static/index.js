2110a8044feba882b9dd5b6421b30988684987e4
thomaswoehlke <thomas.woehlke@gmail.com>
2021-02-12 19:00:38 +0100

    * Fixed #123 split RkiBundeslaenderService into two Services, one for bundeslaender and one for landkreise * Fixed #128 add fields from csv to RkiLandkreiseImport * Fixed #139 refactor RkiBundeslaenderServiceDownload to new method scheme introduced 07.02.2021 * Fixed #140 move WhoImport to RKI in: rk_service_import.py * Fixed #125 implement RkiLandkreise * Fixed #126 implement RkiBundeslaenderImport

--- src/covid19/static/index.js
+++ src/covid19/static/index.js
@@ -1,5 +1,8 @@
 import '/static/node_modules/jquery/dist/jquery.min.js'
 import '/static/node_modules/popper.js/dist/popper.min.js'
 import '/static/node_modules/bootstrap/dist/js/bootstrap.min.js'
-import '/static/node_modules/@fortawesome/fontawesome-free/js/all.min.js'
+import '/static/node_modules/@fortawesome/free-solid-svg-icons/index.js'
+import '/static/node_modules/@fortawesome/free-brands-svg-icons/index.js'
+import '/static/node_modules/@fortawesome/free-regular-svg-icons/index.js'
 import '/static/js/covid19python.js'
+/* */
