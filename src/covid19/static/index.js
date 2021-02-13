10528355b3ed41b13dc6589157e58a5596e199f9
thomaswoehlke <thomas.woehlke@gmail.com>
2021-02-13 13:56:56 +0100

    ## 0.0.15 Release * ------------------------------------- * Fixed #88 rename VaccinationImport to VaccinationImport * Fixed #89 change tablename from vaccination_germany_timeline_import to vaccination_import * Fixed #86 rename VaccinationData to VaccinationData * Fixed #162 rename table vaccination_germany_timeline into vaccination_data * ------------------------------------- * Fixed #170 implement url_vaccination_task_update_star_schema_initial in vaccination_views.py * Fixed #171 implement url_vaccination_task_update_starschema_incremental in vaccination_views.py * Fixed #172 implement url_vaccination_task_import_only in vaccination_views.py * Fixed #173 implement url_vaccination_task_import_only in vaccination_views.py * Fixed #174 implement url_vaccination_task_update_dimensiontables_only in vaccination_views.py * Fixed #175 implement url_vaccination_task_update_facttable_incremental_only in vaccination_views.py * Fixed #176 implement url_vaccination_task_update_facttable_initial_only in vaccination_views.py * ------------------------------------- * Fixed #91 implement VaccinationService.run_download_only * Fixed #92 implement VaccinationService.run_import_only * Fixed #93 implement VaccinationService.run_update_dimension_tables_only * Fixed #94 implement VaccinationService.run_update_fact_table_incremental_only * Fixed #95 implement VaccinationService.run_update_fact_table_initial_only * Fixed #96 implement VaccinationService.run_update_star_schema_incremental * Fixed #97 implement VaccinationService.run_update_star_schema_initial * Fixed #101 implement VaccinationServiceUpdate.update_dimension_tables_only * Fixed #102 implement VaccinationServiceUpdate.update_fact_table_incremental_only * Fixed #103 implement VaccinationServiceUpdate.update_fact_table_initial_only * Fixed #104 implement VaccinationServiceUpdate.update_star_schema_incremental * Fixed #105 implement VaccinationServiceUpdate.update_star_schema_initial * ------------------------------------- * Fixed #90 refactor VaccinationService to new method scheme introduced 07.02.2021 * Fixed #98 refactor VaccinationServiceDownload to new method scheme introduced 07.02.2021 * Fixed #99 refactor VaccinationServiceImport to new method scheme introduced 07.02.2021 * Fixed #100 refactor VaccinationServiceUpdate to new method scheme introduced 07.02.2021 * ------------------------------------- * Fixed #87 change to: Vaccination.datum many to one VaccinationDateReported * Fixed #106 add Tasks and URLs for starting Tasks to vaccination_views * -------------------------------------

--- src/covid19/static/index.js
+++
@@ -1,6 +0,0 @@
-import '/static/vendor/jquery/dist/jquery.min.js'
-import '/static/vendor/popper.js/dist/popper.min.js'
-import '/static/vendor/bootstrap/dist/js/bootstrap.min.js'
-import '/static/vendor/fontawesome/svg-core/index.js'
-import '/static/js/covid19python.js'
-/* */
