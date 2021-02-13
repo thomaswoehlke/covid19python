04b7ef0f12116aa1d5a0ed27ea610921882f7694
thomaswoehlke <thomas.woehlke@gmail.com>
2021-02-13 15:59:36 +0100

    ## 0.0.15 Release * ------------------------------------- * Fixed #88 rename VaccinationImport to VaccinationImport * Fixed #89 change tablename from vaccination_germany_timeline_import to vaccination_import * Fixed #86 rename VaccinationData to VaccinationData * Fixed #162 rename table vaccination_germany_timeline into vaccination_data * ------------------------------------- * Fixed #170 implement url_vaccination_task_update_star_schema_initial in vaccination_views.py * Fixed #171 implement url_vaccination_task_update_starschema_incremental in vaccination_views.py * Fixed #172 implement url_vaccination_task_import_only in vaccination_views.py * Fixed #173 implement url_vaccination_task_import_only in vaccination_views.py * Fixed #174 implement url_vaccination_task_update_dimensiontables_only in vaccination_views.py * Fixed #175 implement url_vaccination_task_update_facttable_incremental_only in vaccination_views.py * Fixed #176 implement url_vaccination_task_update_facttable_initial_only in vaccination_views.py * ------------------------------------- * Fixed #91 implement VaccinationService.run_download_only * Fixed #92 implement VaccinationService.run_import_only * Fixed #93 implement VaccinationService.run_update_dimension_tables_only * Fixed #94 implement VaccinationService.run_update_fact_table_incremental_only * Fixed #95 implement VaccinationService.run_update_fact_table_initial_only * Fixed #96 implement VaccinationService.run_update_star_schema_incremental * Fixed #97 implement VaccinationService.run_update_star_schema_initial * Fixed #101 implement VaccinationServiceUpdate.update_dimension_tables_only * Fixed #102 implement VaccinationServiceUpdate.update_fact_table_incremental_only * Fixed #103 implement VaccinationServiceUpdate.update_fact_table_initial_only * Fixed #104 implement VaccinationServiceUpdate.update_star_schema_incremental * Fixed #105 implement VaccinationServiceUpdate.update_star_schema_initial * ------------------------------------- * Fixed #90 refactor VaccinationService to new method scheme introduced 07.02.2021 * Fixed #98 refactor VaccinationServiceDownload to new method scheme introduced 07.02.2021 * Fixed #99 refactor VaccinationServiceImport to new method scheme introduced 07.02.2021 * Fixed #100 refactor VaccinationServiceUpdate to new method scheme introduced 07.02.2021 * ------------------------------------- * Fixed #87 change to: Vaccination.datum many to one VaccinationDateReported * Fixed #106 add Tasks and URLs for starting Tasks to vaccination_views * -------------------------------------

--- src/covid19/blueprints/common/application_views.py
+++ src/covid19/blueprints/common/application_views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, Blueprint
 from database import app
 from covid19.blueprints.common.application_model_transient import ApplicationPage
 
@@ -17,18 +17,23 @@ from covid19.blueprints.rki_landkreise.rki_views import app_rki_landkreise
 from covid19.blueprints.rki_vaccination.rki_vaccination_views import app_rki_vaccination
 from covid19.blueprints.who.who_views import app_who
 
+
+app_application = Blueprint('application', __name__, template_folder='templates', url_prefix='/')
+
 app.register_blueprint(app_admin, url_prefix='/admin')
+app.register_blueprint(app_application, url_prefix='/')
 app.register_blueprint(app_ecdc, url_prefix='/ecdc')
 app.register_blueprint(app_rki_bundeslaender, url_prefix='/rki/bundeslaender')
 app.register_blueprint(app_rki_landkreise, url_prefix='/rki/landkreise')
 app.register_blueprint(app_rki_vaccination, url_prefix='/rki/vaccination')
 app.register_blueprint(app_who, url_prefix='/who')
 
+
 ############################################################################################
 #
 # WEB
 #
-@app.route('/home')
+@app_application.route('/home')
 def url_home():
     page_info = ApplicationPage('Home', "Covid19 Data")
     return render_template(
@@ -36,6 +41,6 @@ def url_home():
         page_info=page_info)
 
 
-@app.route('/')
+@app_application.route('/')
 def url_root():
     return redirect(url_for('url_home'))
