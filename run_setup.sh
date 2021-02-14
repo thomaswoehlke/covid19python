3b3f10ea3606d031758fdbd508d8c11d026ed842
thomaswoehlke <thomas.woehlke@gmail.com>
2021-02-14 13:52:06 +0100

    * Fixed #123 split RkiBundeslaenderService into two Services, one for bundeslaender and one for landkreise * Fixed #128 add fields from csv to RkiLandkreiseImport * Fixed #139 refactor RkiBundeslaenderServiceDownload to new method scheme introduced 07.02.2021 * Fixed #140 move WhoImport to RKI in: rk_service_import.py * Fixed #125 implement RkiLandkreise * Fixed #126 implement RkiBundeslaenderImport

---
+++ run_setup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+deactivate
+echo "--------------------------------------------------"
+echo "rm -rf venv"
+echo "--------------------------------------------------"
+rm -rf venv
+#python3 -m venv venv
+echo "--------------------------------------------------"
+echo "virtualenv venv"
+echo "--------------------------------------------------"
+virtualenv venv
+echo "--------------------------------------------------"
+echo ". venv/bin/activate"
+echo "--------------------------------------------------"
+. venv/bin/activate
+export FLASK_ENV=development
+export FLASK_APP=covid19
+echo "--------------------------------------------------"
+echo "FLASK_ENV=$FLASK_ENV"
+echo "FLASK_APP=$FLASK_APP"
+echo "--------------------------------------------------"
+echo "pip install -r requirements.txt"
+echo "--------------------------------------------------"
+pip install -r requirements.txt
+echo "--------------------------------------------------"
+echo "pip install -r requirements-tools.txt"
+echo "--------------------------------------------------"
+pip install -r requirements-tools.txt
+echo "--------------------------------------------------"
+echo "pip install -e ."
+echo "--------------------------------------------------"
+pip install -e .
+echo "--------------------------------------------------"
+echo " DONE"
+echo "--------------------------------------------------"
