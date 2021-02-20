816848548f08e1461c3f5a846e21cf4092a62ed5
thomaswoehlke <thomas.woehlke@gmail.com>
2021-02-20 20:22:43 +0100

    working for 0.0.198 Release

---
+++ src/app.py
@@ -0,0 +1,13 @@
+import sys
+import subprocess
+import covid19
+import covid19_worker
+from covid19 import app
+from covid19.blueprints.application.application_workers import run_app
+
+# ---------------------------------------------------------------------------------
+#  MAIN
+# ---------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    run_app(app)
--- src/covid19/blueprints/application/application_workers.py
+++ src/covid19/blueprints/application/application_workers.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 from covid19 import app
-from database import create_celery
+from database import create_celery, run_run_with_debug, port
 
 
 def run_mq(my_app, my_celery):
@@ -18,6 +18,31 @@ def run_mq(my_app, my_celery):
     my_app.logger.info(" ")
     args = ['worker', '-l', 'INFO']
     my_celery.start(args)
+
+
+def run_app(my_app):
+    if sys.platform != 'linux':
+        my_app.logger.info("-------------------------------------------------------------")
+        my_app.logger.info("#                start REDIS-Server                         #")
+        my_app.logger.info("-------------------------------------------------------------")
+        redis_cmd = ['redis-server']
+        subprocess.Popen(redis_cmd, shell=True)
+    my_app.logger.info(" ")
+    my_app.logger.info("#############################################################")
+    my_app.logger.info("#                Covid19 Data - WORKER                      #")
+    my_app.logger.info("#############################################################")
+    my_app.logger.info(" ")
+    celery_cmd = ['celery', 'worker', '-l', 'INFO']
+    subprocess.Popen(celery_cmd, shell=True)
+    my_app.logger.info(" ")
+    my_app.logger.info("#############################################################")
+    my_app.logger.info("#                Covid19 Data - WEB                         #")
+    my_app.logger.info("#############################################################")
+    my_app.logger.info(" ")
+    my_app.run(
+        debug=run_run_with_debug,
+        port=port
+    )
 
 
 celery = create_celery(app)
