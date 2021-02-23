a190aba6b118986769bfc180d963bc7a06c89545
thomas.woehlke <thomas.woehlke@rub.de>
2021-02-23 18:09:01 +0100

    work

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 pip install -r requirements\dev.in --user --force-reinstall --use-pep517 --no-cache-dir --compile --progress-bar pretty --log logfile1.txt
-pip install -e . --implementation py --python-version 3.8 --user --force-reinstall --use-pep517 --no-cache-dir --compile --progress-bar pretty --log logfile2.txt
+pip install -e . --user --force-reinstall --use-pep517 --no-cache-dir --compile --progress-bar pretty --log logfile2.txt
 rem pip install -q build --implementation py  --python-version 3.8 --user --force-reinstall --use-pep517 --compile --progress-bar pretty
 python -m build
 rem pip install -e . --compile --progress-bar pretty
