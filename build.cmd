78c0ea9a9b764f4e40196ffd3aee8b5d073e48a7
thomas.woehlke <thomas.woehlke@rub.de>
2021-02-23 19:03:02 +0100

    work

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
+.\venv\Scripts\activate
 pip install -r requirements\dev.in --user --force-reinstall --use-pep517 --no-cache-dir --compile --progress-bar pretty --log logfile1.txt
 pip install -e . --user --force-reinstall --use-pep517 --no-cache-dir --compile --progress-bar pretty --log logfile2.txt
 rem pip install -q build --implementation py  --python-version 3.8 --user --force-reinstall --use-pep517 --compile --progress-bar pretty
