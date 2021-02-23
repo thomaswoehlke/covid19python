119b13ba9ab8efe8016e7051dec04e0e823d8f87
thomas.woehlke <thomas.woehlke@rub.de>
2021-02-23 15:33:18 +0100

    work

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
-pip install -e . --compile --progress-bar pretty
+pip install -e .
 pip install -q build
 python -m build
+pip install -e . --compile --progress-bar pretty
 pip install dist/artefact_content-0.0.18.whl
