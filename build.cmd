a48b963931b8e0a4e385167e973581dd63d36bb2
thomasoehlke <thomas.woehlke@rub.de>
2021-02-23 18:38:34 +0100

    work

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 pip install -r requirements\dev.in --user --force-reinstall --use-pep517 --no-cache-dir --compile --progress-bar pretty --log logfile1.txt
 pip install -e . --user --force-reinstall --use-pep517 --no-cache-dir --compile --progress-bar pretty --log logfile2.txt
 rem pip install -q build --implementation py  --python-version 3.8 --user --force-reinstall --use-pep517 --compile --progress-bar pretty
-python -m build
+rem repython -m build
 rem pip install -e . --compile --progress-bar pretty
 pip install dist/covid19python-0.0.19.whl --progress-bar pretty --log logfile3.txt
