d33e796b49f125f16d707852bbe90b1859ef536c
thomas.woehlke <thomas.woehlke@rub.de>
2021-02-24 19:35:26 +0100

    Default task

--- build.cmd
+++ build.cmd
@@ -1,7 +1,23 @@
-rem .\venv\Scripts\activate
-pip install -r requirements\dev.in --force-reinstall --use-pep517 --no-cache-dir --compile --progress-bar pretty --log logfile1.txt
-pip install -e . --force-reinstall --use-pep517 --no-cache-dir --compile --progress-bar pretty --log logfile2.txt
+rem deactivate
+rem rm -rf venv
+rem py -3 -m venv venv
+rem venv\Scripts\activate
+
+pip-compile -r requirements\build.in
+pip-compile -r requirements\docs.in
+pip-compile -r requirements\tests.in
+pip-compile -r requirements\dev.in
+
+pip install -r requirements\build.in  --log logfile1.txt
+pip install -r requirements\docs.in  --log logfile2.txt
+pip install -r requirements\tests.in --log logfile3.txt
+pip install -r requirements\dev.in --log logfile4.txt
+
+rem pip install -r requirements\dev.in --force-reinstall --use-pep517 --no-cache-dir --compile --progress-bar pretty --log logfile1.txt
+rem pip install -r requirements\dev.in --use-pep517 --progress-bar pretty --log logfile1.txt
+rem pip install -e . --force-reinstall --use-pep517 --no-cache-dir --compile --progress-bar pretty --log logfile2.txt
 rem pip install -q build --implementation py  --python-version 3.8 --user --force-reinstall --use-pep517 --compile --progress-bar pretty
-rem python -m build
-rem pip install -e . --compile --progress-bar pretty
-pip install dist/covid19python-0.0.19.whl --progress-bar pretty --log logfile3.txt
+
+pip install -e . --compile --force-reinstall --progress-bar pretty --log logfile5.txt
+
+python -m build --wheel
