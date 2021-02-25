76403b9461e98dfa51cb8b58f30ad12455670504
thomas.woehlke <thomas.woehlke@rub.de>
2021-02-25 11:52:50 +0100

    Default task

--- build.cmd
+++ build.cmd
@@ -13,7 +13,7 @@ pip install -r requirements\docs.in  --log logfile2.txt
 pip install -r requirements\tests.in --log logfile3.txt
 pip install -r requirements\dev.in --log logfile4.txt
 
-rem pip install -e . --compile --force-reinstall --progress-bar pretty --log logfile5.txt
-pip install -e . --compile --progress-bar pretty --log logfile5.txt
+rem rem pip install -e . --compile --force-reinstall --progress-bar pretty --log logfile5.txt
+rem pip install -e . --compile --progress-bar pretty --log logfile5.txt
 
 rem python -m build --wheel
