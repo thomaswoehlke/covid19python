b28b02a15305a8d6cb78df1dab58fdb8bab95bbe
thomaswoehlke <thomas.woehlke@gmail.com>
2021-02-26 11:30:06 +0100

    work

--- build.sh
+++ build.sh
@@ -1,21 +1,42 @@
-# deactivate
-# rm -rf venv
-# py -3 -m venv venv
-# venv\Scripts\activate
+#!/usr/bin/env bash
 
-pip-compile -r requirements/build.in
-pip-compile -r requirements/docs.in
-pip-compile -r requirements/tests.in
-pip-compile -r requirements/dev.in
+function setup_venv() {
+    deactivate
+    rm -rf venv
+    py -3 -m venv venv
+    venv/Scripts/activate
+}
 
-pip install -r requirements/build.in  --log logfile1.txt
-pip install -r requirements/docs.in  --log logfile2.txt
-pip install -r requirements/tests.in --log logfile3.txt
-pip install -r requirements/dev.in --log logfile4.txt
+function pip_compile() {
+  pip-compile -r requirements/build.in
+  pip-compile -r requirements/docs.in
+  pip-compile -r requirements/tests.in
+  pip-compile -r requirements/dev.in
+  . scripts/script_get_python_requirements_from_txt.sh
+}
 
-. scripts/script_get_python_requirements_from_txt.sh
+function pip_install() {
+  pip install -r requirements/build.in  --log logfile1.txt
+  pip install -r requirements/docs.in  --log logfile2.txt
+  pip install -r requirements/tests.in --log logfile3.txt
+  pip install -r requirements/dev.in --log logfile4.txt
+}
 
-# pip install -e . --compile --force-reinstall --progress-bar pretty --log logfile5.txt
-pip install -e . --compile --progress-bar pretty --log logfile5.txt
+function pip_install_via_setup_py() {
+  # pip install -e . --compile --force-reinstall --progress-bar pretty --log logfile5.txt
+  pip install -e . --compile --progress-bar pretty --log logfile5.txt
+}
 
-# python -m build --wheel
+function build_wheel() {
+  python -m build --wheel
+}
+
+function main() {
+  #setup_venv
+  pip_compile
+  pip_install
+  pip_install_via_setup_py
+  #build_wheel
+}
+
+main
--- scripts/script_get_python_requirements_from_txt.sh
+++ scripts/script_get_python_requirements_from_txt.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 cat requirements/build.txt | grep -v '#' | sed 's/^/\t"/g' | sed 's/$/",/g' > requirements/req_build.py
 cat requirements/docs.txt | grep -v '#' | sed 's/^/\t"/g' | sed 's/$/",/g' > requirements/req_docs.py
 cat requirements/tests.txt | grep -v '#' | sed 's/^/\t"/g' | sed 's/$/",/g' > requirements/req_tests.py
