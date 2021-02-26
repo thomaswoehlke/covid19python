67b285e439be7676cfa840f8d84a26acb03ea9fc
thomaswoehlke <thomas.woehlke@gmail.com>
2021-02-26 13:08:57 +0100

    work

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ function pip_compile() {
   pip-compile -r requirements/tests.in
   pip-compile -r requirements/dev.in
   . scripts/script_get_python_requirements_from_txt.sh
+  pip check
 }
 
 function pip_install() {
@@ -21,19 +22,22 @@ function pip_install() {
   pip install -r requirements/docs.in  --log logfile2.txt
   pip install -r requirements/tests.in --log logfile3.txt
   pip install -r requirements/dev.in --log logfile4.txt
+  pip check
 }
 
 function pip_install_via_setup_py() {
   pip install -e . --compile --force-reinstall --progress-bar pretty --log logfile5.txt
   # pip install -e . --compile --progress-bar pretty --log logfile5.txt
+  pip check
 }
 
 function build_wheel() {
-  python -m build --wheel
+  #python -m build --wheel
+  pip check
 }
 
 function main() {
-  #setup_venv
+  # setup_venv
   pip_compile
   pip_install
   pip_install_via_setup_py
