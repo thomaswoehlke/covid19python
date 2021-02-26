92b7865f62c9d372cce3942bbdfe9c51d90d2931
thomaswoehlke <thomas.woehlke@gmail.com>
2021-02-26 12:30:05 +0100

    work

--- scripts/scipt_get_python_requirements_from_txt.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cat requirements/build.txt | grep -v '#' | sed 's/^/\t"/g' | sed 's/$/",/g' > requirements/req_build.py
-cat requirements/docs.txt | grep -v '#' | sed 's/^/\t"/g' | sed 's/$/",/g' > requirements/req_docs.py
-cat requirements/tests.txt | grep -v '#' | sed 's/^/\t"/g' | sed 's/$/",/g' > requirements/req_tests.py
-cat requirements/dev.txt | grep -v '#' | sed 's/^/\t"/g' | sed 's/$/",/g' > requirements/req_dev.py
--- scripts/script_get_python_requirements_from_txt.py
+++ scripts/script_get_python_requirements_from_txt.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 
 def get_python_requirements_from_txt():
-    my_cmd = ['bash', 'scripts'+os.sep+'scipt_get_python_requirements_from_txt.sh']
+    my_cmd = ['bash', 'scripts'+os.sep+'script_get_python_requirements_from_txt.sh']
     returncode = subprocess.call(my_cmd, shell=True)
     if returncode == 0:
         logging.info("retcode: "+str(returncode))
