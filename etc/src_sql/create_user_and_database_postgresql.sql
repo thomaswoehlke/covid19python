38d5931533475f34d2b8b86823d6ad8106035b78
thomaswoehlke <thomas.woehlke@gmail.com>
2021-04-06 11:07:38 +0200

    setup

--- etc/src_sql/create_user_and_database_postgresql.sql
+++ etc/src_sql/create_user_and_database_postgresql.sql
@@ -29,3 +29,27 @@ CREATE DATABASE covid19data
     LC_CTYPE = 'de_DE.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
+
+
+
+
+CREATE ROLE tw WITH
+	LOGIN
+	SUPERUSER
+	CREATEDB
+	CREATEROLE
+	INHERIT
+	REPLICATION
+	CONNECTION LIMIT -1
+	PASSWORD 'Recoil89';
+GRANT pg_execute_server_program, pg_monitor, pg_read_all_settings, pg_read_all_stats, pg_read_server_files, pg_signal_backend TO tw WITH ADMIN OPTION;
+
+CREATE DATABASE tw
+    WITH
+    OWNER = tw
+    TEMPLATE = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'de_DE.UTF-8'
+    LC_CTYPE = 'de_DE.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
