dc3485eefe49870ab822c161a2e81feb97017e0b
thomaswoehlke <thomas.woehlke@gmail.com>
2021-04-06 16:34:43 +0200

    template changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 from setuptools import find_packages, setup
 
-version = '0.0.22'
+version = '0.0.23'
 
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
