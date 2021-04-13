4739bd75cddbe4c495a9da557cb8fa56835b1e6c
thomaswoehlke <thomas.woehlke@gmail.com>
2021-04-13 15:37:12 +0200

    uses cases

---
+++ src/helper.py
@@ -0,0 +1,3 @@
+for i in range(200, 356):
+    msg = 'au --> UC'+str(i)
+    print(msg)
--- src/run4test_mq.py
+++ src/run4test_mq.py
@@ -1,7 +1,6 @@
 import sys
 import subprocess
 import covid19
-import covid19_worker
 from covid19 import app
 from covid19.blueprints.application.application_workers import celery, run_mq
 
